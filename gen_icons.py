853e393f50c94ebe451a8ea3ca3ed19ef6ccbba5
Joshua VonFricken <jovonfricken@gmail.com>
2019-01-01 16:41:32 -0500

    Actually adding script

--- gen_icons.py
+++ gen_icons.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", help="file name of icon")
 parser.add_argument("-n", "--newfile",
                     help="overrides the file name during saving")
+parser.add_argument("-c", "--config", help="utilizes config file to generate icons and icon names", action="store_true")
 
 
 def get_file_name():
@@ -19,20 +20,41 @@ def get_file_name():
     if args.file:
         return args.file, args.file
 
+def use_config_file():
+    args = parser.parse_args()
 
-def main():
-    file_name, new_file_name = get_file_name()
+    return args.config
 
-    image = Image.open(file_name)
+def generate_icons_config(image):
 
+    config = {}
+    with open("config.json", "r") as file:
+        data = file.read()
+
+        config = json.loads(data)
+
+    for size in config["sizes"]:
+        resized_image = image.resize((size["res"], size["res"]), Image.LANCZOS)
+        resized_image.save(size["name"])
+
+def generate_icons(file_name, new_file_name, image):
     image_width, image_height = image.size
 
     for i in range(1, 4):
-        devisor = 4 - i
-        resized_image = image.resize(
-            (int(image_width/devisor), int(image_height/devisor)), Image.LANCZOS)
+        height = width = int((image_width/3)*i)
+
+        resized_image = image.resize((width, height), Image.LANCZOS)
         resized_image.save(new_file_name.replace(
             ".png", "@{size}x.png".format(size=str(i))))
+
+def main():
+    file_name, new_file_name = get_file_name()
+    image = Image.open(file_name)
+    if use_config_file:
+        generate_icons_config(image)
+    else:
+        generate_icons(file_name, new_file_name, image)
+
 
 
 if __name__ == "__main__":
